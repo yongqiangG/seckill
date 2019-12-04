ffe0c226d13feb514a12d00012020682abbecc80
Johnny <johnny.gor10@gmail.com>
2019-12-04 16:57:32 +0800

    web层编写

--- src/main/java/com/johnny/controller/SecKillController.java
+++ src/main/java/com/johnny/controller/SecKillController.java
@@ -1,4 +1,129 @@
 package com.johnny.controller;
 
+import com.johnny.dto.Exposer;
+import com.johnny.dto.SecKillExecution;
+import com.johnny.dto.SecKillResult;
+import com.johnny.entity.SecKill;
+import com.johnny.enums.SecKillStateEnum;
+import com.johnny.exception.RepeatKillException;
+import com.johnny.exception.SecKillCloseException;
+import com.johnny.exception.SecKillException;
+import com.johnny.service.SecKillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+//符合Restful规范,/模块/资源/{id}/操作(名词)
+@RequestMapping("/seckill")
 public class SecKillController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private SecKillService secKillService;
+
+    /**
+     * 秒杀列表页
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("/list")
+    public String list(Model model) {
+        List<SecKill> list = secKillService.getSecKillList();
+        model.addAttribute("list", list);
+        //返回WEB-INF/jsp/list.jsp
+        return "list";
+    }
+
+    /**
+     * 秒杀详情页
+     *
+     * @param secKillId
+     * @param model
+     * @return
+     */
+    @RequestMapping("/{secKillId}/detail")
+    public String detail(@PathVariable("secKillId") Long secKillId, Model model) {
+        if (secKillId == null) {
+            return "redirect:/seckill/list";
+        }
+        SecKill secKill = secKillService.getById(secKillId);
+        if (secKill == null) {
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("secKill", secKill);
+        return "detail";
+    }
+
+    /**
+     * 暴露秒杀接口
+     */
+    @RequestMapping(value = "/{secKillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SecKillResult<Exposer> exposer(@PathVariable("secKillId") Long secKillId) {
+        Exposer exposer = secKillService.exportSecKillUrl(secKillId);
+        SecKillResult<Exposer> result;
+        if (exposer.isExposed()) {
+            result = new SecKillResult<>(true, exposer);
+            return result;
+        }
+        result = new SecKillResult<>(false, exposer);
+        return result;
+    }
+
+    /**
+     * 秒杀执行结果
+     */
+    @RequestMapping(value = "/{secKillId}/{md5}/execution",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"}
+    )
+    @ResponseBody
+    public SecKillResult<SecKillExecution> execute(@PathVariable("secKillId") Long secKillId,
+                                                   @CookieValue(value = "killPhone") Long userPhone,
+                                                   @PathVariable("md5") String md5) {
+        SecKillResult<SecKillExecution> result;
+        if (userPhone == null) {
+            return new SecKillResult<SecKillExecution>(false, "该手机号码未注册");
+        }
+        try {
+            SecKillExecution secKillExecution = secKillService.executeSecKill(secKillId, userPhone, md5);
+            result = new SecKillResult<>(true, secKillExecution);
+            return result;
+        } catch (RepeatKillException e) {
+            SecKillExecution secKillExecution = new SecKillExecution(secKillId, SecKillStateEnum.REPEAT_KILL);
+            result = new SecKillResult<>(false, secKillExecution);
+            return result;
+        } catch (SecKillCloseException e) {
+            SecKillExecution secKillExecution = new SecKillExecution(secKillId, SecKillStateEnum.END);
+            result = new SecKillResult<>(false, secKillExecution);
+            return result;
+        } catch (SecKillException e) {
+            SecKillExecution secKillExecution = new SecKillExecution(secKillId, SecKillStateEnum.DATA_REWRITE);
+            result = new SecKillResult<>(false, secKillExecution);
+            return result;
+        }
+    }
+
+    /**
+     * 服务器当前时间
+     */
+    @RequestMapping(value = "/nowTime",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SecKillResult<Long> nowTime() {
+        Date date = new Date();
+        SecKillResult<Long> result;
+        result = new SecKillResult<>(true, date.getTime());
+        return result;
+    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/detail.jsp
@@ -0,0 +1,60 @@
+<%@page contentType="text/html; charset=UTF-8" language="java" %>
+<%@include file="tag.jsp"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>${secKill.name}</title>
+    <%@include file="head.jsp"%>
+</head>
+<body>
+    <div class="container">
+        <div class="panel panel-default text-center">
+            <div class="panel-heading">
+                <h1>${secKill.name}</h1>
+            </div>
+            <div class="panel-body">
+                <h2 class="text-danger">
+                    <%--显示time图标--%>
+                    <span class="glyphicon glyphicon-time"></span>
+                    <span class="glyphicon" id="secKill-box"></span>
+                </h2>
+            </div>
+        </div>
+    </div>
+
+    <%--电话注册模态框--%>
+    <div id="killPhoneModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title text-center">
+                        <span class="glyphicon glyphicon-phone"></span>
+                    </h3>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-xs-8 col-xs-offset-2">
+                            <input type="text" name="killPhone" id="killPhone" placeholder="请输入您的手机号" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <span id="killPhoneMessage" class="glyphicon"></span>
+                    <button type="button" id="killPhoneBtn" class="btn btn-success">
+                        <span class="glyphicon glyphcon-phone"></span>
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
+<!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
+<%--cookie操作插件--%>
+<script src="http://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<%--倒计时插件--%>
+<script src="http://cdn.bootcss.com/jquery-countdown/2.1.0/jquery.countdown.min.js"></script>
+</html>
